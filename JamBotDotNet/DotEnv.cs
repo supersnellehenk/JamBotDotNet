ccf1e4454a043126b899edc5ceafac8d5abdfaab
supersnellehenk <rick@marojeri.nl>
2022-11-08 22:43:12 +0100

    Bot setup

--- JamBotDotNet/DotEnv.cs
+++ JamBotDotNet/DotEnv.cs
@@ -1,6 +1,16 @@
 ﻿namespace JamBotDotNet;
 
-public class DotEnv
+public static class DotEnv
 {
-    
+    public static void Load()
+    {
+        var lines = File.ReadAllLines(".env");
+        foreach (var line in lines)
+        {
+            var parts = line.Split('=');
+            var key = parts[0];
+            var value = parts[1];
+            Environment.SetEnvironmentVariable(key, value);
+        }
+    }
 }
